a5dc9a84fa18a5bc2e75bf8a5cf336fb150be69a
Enrique Lacal <el00244@surrey.ac.uk>
2019-03-03 12:03:52 +0000

    Add demo.py

---
+++ samples/demo.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mask R-CNN Demo
+# 
+# A quick intro to using the pre-trained model to detect and segment objects.
+
+# In[1]:
+
+
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+# Import COCO config
+sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+import coco
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+# Download COCO trained weights from Releases if needed
+if not os.path.exists(COCO_MODEL_PATH):
+    utils.download_trained_weights(COCO_MODEL_PATH)
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "images")
+
+
+# ## Configurations
+# 
+# We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
+# 
+# For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
+
+# In[2]:
+
+
+class InferenceConfig(coco.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+config = InferenceConfig()
+config.display()
+
+
+# ## Create Model and Load Trained Weights
+
+# In[3]:
+
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+
+# ## Class Names
+# 
+# The model classifies objects and returns class IDs, which are integer value that identify each class. Some datasets assign integer values to their classes and some don't. For example, in the MS-COCO dataset, the 'person' class is 1 and 'teddy bear' is 88. The IDs are often sequential, but not always. The COCO dataset, for example, has classes associated with class IDs 70 and 72, but not 71.
+# 
+# To improve consistency, and to support training on data from multiple sources at the same time, our ```Dataset``` class assigns it's own sequential integer IDs to each class. For example, if you load the COCO dataset using our ```Dataset``` class, the 'person' class would get class ID = 1 (just like COCO) and the 'teddy bear' class is 78 (different from COCO). Keep that in mind when mapping class IDs to class names.
+# 
+# To get the list of class names, you'd load the dataset and then use the ```class_names``` property like this.
+# ```
+# # Load COCO dataset
+# dataset = coco.CocoDataset()
+# dataset.load_coco(COCO_DIR, "train")
+# dataset.prepare()
+# 
+# # Print class names
+# print(dataset.class_names)
+# ```
+# 
+# We don't want to require you to download the COCO dataset just to run this demo, so we're including the list of class names below. The index of the class name in the list represent its ID (first class is 0, second is 1, third is 2, ...etc.)
+
+# In[4]:
+
+
+# COCO Class names
+# Index of the class in the list is its ID. For example, to get ID of
+# the teddy bear class, use: class_names.index('teddy bear')
+class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+               'bus', 'train', 'truck', 'boat', 'traffic light',
+               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
+               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
+               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
+               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+               'kite', 'baseball bat', 'baseball glove', 'skateboard',
+               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
+               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
+               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
+               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
+               'teddy bear', 'hair drier', 'toothbrush']
+
+
+# ## Run Object Detection
+
+# In[5]:
+
+
+# Load a random image from the images folder
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+print(image.shape)
+# Run detection
+results = model.detect([image], verbose=1)
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], 
+                            class_names, r['scores'])
+
+
+# In[ ]:
+
+
+import cv2 as cv
+from PIL import Image
+import IPython
+
+"""def vis_segmentation_stream(image, seg_map, index):
+    #Visualizes segmentation overlay view and stream it with IPython display.
+    plt.figure(figsize=(12, 7))
+
+    seg_image = label_to_color_image(seg_map).astype(np.uint8)
+    plt.imshow(image)
+    plt.imshow(seg_image, alpha=0.7)
+    plt.axis('off')
+    plt.title('segmentation overlay | frame #%d'%index)
+    plt.grid('off')
+    plt.tight_layout()
+
+    # Show visualization in a streaming fashion.
+    f = BytesIO()
+    plt.savefig(f, format='jpeg')
+    IPython.display.display(IPython.display.Image(data=f.getvalue()))
+    f.close()
+    plt.close()
+"""
+def run_visualization_video(frame, index, output_video):
+    """Inferences Mask R-CNN model on a video file and stream the visualization."""
+    original_im = frame[..., ::-1]
+    #seg_map = MODEL.run(original_im)
+    #vis_segmentation_stream(original_im, seg_map, index) 
+    
+    # Run detection
+    results = model.detect([original_im], verbose=1)
+    
+    # Visualize results
+    r = results[0]
+  
+    #print(r["masks"])
+    print("Visualizing...")
+    img = visualize.display_instances(original_im, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'], return_image=True)
+    print(img)
+    output_video.write(img)
+
+SAMPLE_VIDEO = '/Users/Enrique/research/mit-deep-learning/tutorial_driving_scene_segmentation/mit_driveseg_sample.mp4'
+#SAMPLE_VIDEO = urllib.request.urlretrieve('https://github.com/lexfridman/mit-deep-learning/raw/master/tutorial_driving_scene_segmentation/mit_driveseg_sample.mp4')[0]
+print('running Mask R-CNN on the sample video...')
+
+video = cv.VideoCapture(SAMPLE_VIDEO)
+output_video = cv.VideoWriter("mrcnn_2.mp4", -1, 30, (1920, 1080))
+
+# num_frames = 598  # uncomment to use the full sample video
+num_frames = 598
+
+try:
+    for i in range(num_frames):
+        _, frame = video.read()
+        if not _: break
+        run_visualization_video(frame, i, output_video)
+        IPython.display.clear_output(wait=True)
+except KeyboardInterrupt:
+    plt.close()
+    print("Stream stopped.")
+
+output_video.release()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
